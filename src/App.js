9977426c72c02d6cc3b300e1a26e48a75436b3b7
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2024-01-17 23:01:37 +0530

    Major Changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {NavBar} from "./Components/NavBar";
+import './App.scss'
+import { Route , Routes }  from 'react-router-dom'
+import Layout from './Components/Layouts'
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-    </div>
+      <>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+        </Route>
+      </Routes>
+      </>
   );
 }
 
---
+++ src/Components/Layouts/index.js
@@ -0,0 +1,9 @@
+import Sidebar from "../Sidebar";
+import "./index.scss";
+
+const Layout = () => {
+    return <>
+    <Sidebar></Sidebar></>
+} 
+
+export default Layout
