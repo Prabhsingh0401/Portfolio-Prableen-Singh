6165dfd5643570f4bea434462358ea189338c8b5
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2024-01-25 22:19:05 +0530

    Last Commit

--- src/Components/Home/Logo/index.js
+++ src/Components/Home/Logo/index.js
@@ -2,7 +2,6 @@ import './index.scss'
 import LogoS from '../../../assets/images/Side Logo Solid.png'
 import gsap from 'gsap'
 import { DrawSVGPlugin } from 'gsap-trial/DrawSVGPlugin'
-import LogoSOut from '../../../assets/images/Side Logo Outline.png'
 import { useEffect, useRef } from 'react'
 
 const Logo = () =>{
@@ -26,7 +25,11 @@ const Logo = () =>{
 
     return(
     <div className='Logo-container' ref={bgref}>
-        <img ref={solidLogoRef} className='solid-logo' src={LogoS}></img>
+        <img ref={solidLogoRef} className='solid-logo' src={LogoS} alt='Logo'></img>
+        <svg ref={outlineLogoRef} xmlns="http://www.w3.org/2000/svg" width="1339" height="1236" viewBox="0 0 1339 1236">
+        <text className='cls-1' transform="translate(659.506 1187.235) scale(20.417)"><tspan x="-30">Ps</tspan></text>
+        </svg>
+
     </div>
     )
 }
