e2dfe7ad54d34da34e39eca204f789d465f47aef
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2024-01-28 19:01:18 +0530

    Update index.js

--- src/Components/Home/Logo/index.js
+++ src/Components/Home/Logo/index.js
@@ -1,25 +1,9 @@
 import './index.scss'
-import gsap from 'gsap'
-import { DrawSVGPlugin } from 'gsap-trial/DrawSVGPlugin'
-import { useEffect, useRef } from 'react'
+import {useRef } from 'react'
 
 const Logo = () =>{
     const bgref = useRef();
     const outlineLogoRef = useRef();
-    useEffect(() => {
-        gsap.registerPlugin(DrawSVGPlugin)
-
-        gsap.timeline()
-        .to(bgref.current,{
-            duration:1,
-            opacity:1,
-        })
-        .from(outlineLogoRef.current,{
-            drawSVG:0,
-            duration:20,
-        })
-    },[])
-
     return(
     <div className='Logo-container' ref={bgref}>
         <svg className='sv1' ref={outlineLogoRef} xmlns="http://www.w3.org/2000/svg" width="1339" height="1236" viewBox="0 0 1339 1236">
