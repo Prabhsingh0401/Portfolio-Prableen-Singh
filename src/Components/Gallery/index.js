051463d48eab8e5c0b6099cee49aebcb4254f293
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2024-02-20 23:43:56 +0530

    Gallery Section almost complete

--- src/Components/Gallery/index.js
+++ src/Components/Gallery/index.js
@@ -1,8 +1,15 @@
 import "./index.scss"
+import Image1 from "../../assets/images/PXL_20240206_122231431.jpg"
+import Image2 from "../../assets/images/PXL_20240203_092223168.jpg"
+import Image3 from "../../assets/images/PXL_20240203_091834646.jpg"
 
 const Gallery = () => {
     return(
-        <> Hello </>
+        <>
+        <img className="Image1" src={Image1} alt="Image1"></img>
+        <img className="Image2" src={Image2} alt="Image2"></img>
+        <img className="Image3" src={Image3} alt="Image3"></img>
+        </>
     )
 }
 
