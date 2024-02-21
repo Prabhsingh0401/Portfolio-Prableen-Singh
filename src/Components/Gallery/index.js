db72b767da2f8a3c5ac88e5494af24811447b983
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2024-02-21 23:32:14 +0530

    Gallery Section made pretty

--- src/Components/Gallery/index.js
+++ src/Components/Gallery/index.js
@@ -2,6 +2,9 @@ import "./index.scss"
 import Image1 from "../../assets/images/PXL_20240206_122231431.jpg"
 import Image2 from "../../assets/images/PXL_20240203_092223168.jpg"
 import Image3 from "../../assets/images/PXL_20240203_091834646.jpg"
+import Image4 from "../../assets/images/PXL_20231215_015848913.jpg"
+import Image5 from "../../assets/images/PXL_20231207_131617597.jpg"
+
 
 const Gallery = () => {
     return(
@@ -9,6 +12,11 @@ const Gallery = () => {
         <img className="Image1" src={Image1} alt="Image1"></img>
         <img className="Image2" src={Image2} alt="Image2"></img>
         <img className="Image3" src={Image3} alt="Image3"></img>
+        <img className="Image4" src={Image4} alt="Image4"></img>
+        <img className="Image5" src={Image5} alt="Image4"></img>
+        <p className="Quote1">"Capturing moments that tell <br></br> a thousand words."</p>
+        <p className="Quote2">"Through the lens,<br></br> we find beauty <br></br> in the ordinary."</p>
+
         </>
     )
 }
