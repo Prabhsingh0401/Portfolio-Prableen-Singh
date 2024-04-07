7bf88d0f26361b49bb4c6b487b5c2e04cda55b03
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2024-04-07 22:45:36 +0530

    Project page

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -5,9 +5,18 @@ const Project = () =>{
         <>
         <div className='main-project-page'>
             <div className="container">
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
+                <div className="box">
+                    <h1>Mobihunt</h1>
+                    <p>MobiHunt: A personalized mobile advisor. Answer questions, get tailored recommendations. Find your perfect phone match effortlessly. Let's hunt your ideal device</p>
+                </div>
+                <div className="box">
+                    <h1>TopperHQ</h1>
+                    <p>A website which displays all important QnA from each chapter of every subject in a summarised and easy to remember format.</p>
+                </div>
+                <div className="box">
+                    <h1>SahaytaHUB</h1>
+                    <p>A Website to get donation or investment being a NGO or any Private Firm build at IIIT Hackathon</p>
+                </div>
                 <div className="box"></div>
             </div>
         </div>
