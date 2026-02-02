668ce11464fa04f606d2ffa9235fdaa4e80a2889
Prableen Singh <130837233+Prabhsingh0401@users.noreply.github.com>
2026-02-02 12:18:36 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <div className="footer">
         <footer className="m-4 dark:bg-gray-800">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="#Home" className="hover:underline">Prabh's Portfolio™</a>. All Rights Reserved.
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400"> 2026 <a href="#Home" className="hover:underline">Prableen's Portfolio</a>.
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
